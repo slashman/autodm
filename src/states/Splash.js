4e16246f13151ae0dac6f53472f74066f3e489d0
slash <slash@slashware.net>
2019-03-10 11:56:11 -0500

    Center loading bar

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -6,6 +6,9 @@ export default class extends Phaser.State {
   preload () {
     this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
     this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
+    this.loaderBg.anchor.setTo(0.5)
+    this.loaderBar.anchor.setTo(0.5)
+
     this.load.setPreloadSprite(this.loaderBar)
     
     this.load.image('blank', 'assets/images/blank.png')
