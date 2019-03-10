9969d75b224acf01743b459706b7ad26243d6a31
slash <slash@slashware.net>
2019-03-10 11:25:11 -0500

    Add test locations

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,65 +1,14 @@
 export default [
-  {
-    id: 'lugunum',
-    name: 'Lugunum',
-    type: 'town',
-    x: 2599,
-    y: 3199,
-    connections: [
-      {
-        to: 'lugunumForest'
-      }
-    ]
-  },
-  {
-    id: 'lugunumForest',
-    name: 'Lugunum Forest',
-    type: 'forest',
-    x: 2576,
-    y: 3247,
-    connections: [
-      {
-        to: 'agrorum'
-      },
-      {
-        to: 'borumForest'
-      }
-    ]
-  },
-  {
-    id: 'agrorum',
-    name: 'Agrorum',
-    type: 'town',
-    x: 2622,
-    y: 3243
-  },
-  {
-    id: 'borumForest',
-    name: 'Borum Forest',
-    type: 'forest',
-    x: 2529,
-    y: 3275,
-    connections: [
-      {
-        to: 'aqugus'
-      },
-      {
-        to: 'borum'
-      }
-    ]
-  },
-  {
-    id: 'aqugus',
-    name: 'Aqugus',
-    type: 'town',
-    x: 2643,
-    y: 3380
-  },
-  {
-    id: 'borum',
-    name: 'Borum',
-    type: 'town',
-    x: 2472,
-    y: 3266
-  },
+{ id: "sulleville", name: "Sulleville", type: "town", x: 2475, y: 628, connections: [{ to: "coure"},{ to: "northern-villesia-forest"},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "coure", name: "Coure", type: "town", x: 2430, y: 720, connections: [{ to: "gauzozie"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "gauzozie", name: "Gauzozie", type: "town", x: 2393, y: 766, connections: [{ to: "bevilleres"},{ to: "vignes-sea"},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "northern-villesia-forest", name: "Northern Villesia Forest", type: "forest", x: 2654, y: 677, connections: [{ to: "vignes"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "vignes", name: "Vignes", type: "town", x: 2849, y: 843, connections: [{ to: "chavantray"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "bevilleres", name: "Bevilleres", type: "town", x: 2367, y: 829, connections: [{ to: "bevilleres-meres-crossroad-1"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "bevilleres-meres-crossroad-1", name: "Bevilleres Meres crossroad 1", type: "crossroad", x: 2428, y: 920, connections: [{ to: "meres-forest"},{ to: "charieaux-forest"},{ to: "terteau-crossroad"},{ to: "sanarcy-crossroad"},{ to: ""},]}, 
+{ id: "meres-forest", name: "Meres Forest", type: "forest", x: 2456, y: 876, connections: [{ to: "boescoux"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "boescoux", name: "Boescoux", type: "town", x: 2526, y: 858, connections: [{ to: "bachy"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "bachy", name: "Bachy", type: "town", x: 2501, y: 810, connections: [{ to: ""},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "charieaux-forest", name: "Charieaux Forest", type: "forest", x: 2378, y: 895, connections: [{ to: "charmont-crossroad"},{ to: ""},{ to: ""},{ to: ""},{ to: ""},]}, 
+{ id: "charmont-crossroad", name: "Charmont Crossroad", type: "crossroad", x: 2337, y: 866, connections: [{ to: "charmont"},{ to: "charieaux-forest-2"},{ to: ""},{ to: ""},{ to: ""},]}, 
 ];
